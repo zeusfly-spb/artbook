7f7dc54e5acd1edc9ef5ef0f0b969fd1f0d0a267
Oleg Saifutdinov <zeusfly@mail.ru>
2021-01-23 17:01:48 +0300

    Creating Thumbnail some features

--- app/Article.php
+++ app/Article.php
@@ -11,9 +11,16 @@ class Article extends Model
 
     protected $dates = ['published_at'];
 
+    protected $appends = ['header'];
+
     public function tags()
     {
         return $this->hasMany(Tag::class);
+    }
+
+    public function getHeaderAttribute()
+    {
+        return explode('.', $this->text)[0];
     }
 
     protected static function boot()
--- artbook-front/src/components/Thumbnail.vue
+++ artbook-front/src/components/Thumbnail.vue
@@ -1,7 +1,29 @@
 <template>
-    <v-row>
-        {{ article.slug }}
-    </v-row>
+    <v-card>
+        <v-card-text
+            class="d-inline-flex pa-2"
+            outlined
+            tile
+        >
+            <v-img
+                max-height="150"
+                max-width="150"
+                min-height="150"
+                min-width="150"
+                src="https://via.placeholder.com/50"
+            />
+            <f-flex
+                class="ml-1 flex-column"
+            >
+                <h2>{{ article.header }}</h2>
+                <span
+                    class="caption"
+                >
+                    {{ article.text }}
+                </span>
+            </f-flex>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
